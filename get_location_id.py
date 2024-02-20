2d81ae629540200acebd0b3eeacd13f3c81a4b3c
vu.lh <vu.lh@teko.vn>
2024-02-20 13:56:23 +0700

    handle except

--- get_location_id.py
+++ get_location_id.py
@@ -8,16 +8,19 @@ def get_attraction_location_id(city):
 
     location_ids = []
     for file_name in dir_list:
-        f = open(f"{path}/{file_name}")
-        data = json.load(f)
-        ids = []
-        for item in data:
-            info = item.get("singleFlexCardContent")
-            if info is None:
-                continue
-            if info.get("saveId") is not None and info.get("saveId").get("id") is not None:
-                ids.append(int(info.get("saveId").get("id")))
-        location_ids += ids
+        try:
+            f = open(f"{path}/{file_name}")
+            data = json.load(f)
+            ids = []
+            for item in data:
+                info = item.get("singleFlexCardContent")
+                if info is None:
+                    continue
+                if info.get("saveId") is not None and info.get("saveId").get("id") is not None:
+                    ids.append(int(info.get("saveId").get("id")))
+            location_ids += ids
+        except:
+            print(f"err {city} attractions")
 
     return location_ids
 
@@ -28,13 +31,16 @@ def get_hotels_location_id(city):
 
     location_ids = []
     for file_name in dir_list:
-        f = open(f"{path}/{file_name}")
-        data = json.load(f)
-        ids = []
-        for item in data:
-            if item.get("locationId") is not None:
-                ids.append(int(item.get("locationId")))
-        location_ids += ids
+        try:
+            f = open(f"{path}/{file_name}")
+            data = json.load(f)
+            ids = []
+            for item in data:
+                if item.get("locationId") is not None:
+                    ids.append(int(item.get("locationId")))
+            location_ids += ids
+        except:
+            print(f"err {city} hotels")
 
     return location_ids
 
@@ -45,20 +51,22 @@ def get_restaurants_location_id(city):
 
     location_ids = []
     for file_name in dir_list:
-        f = open(f"{path}/{file_name}")
-        data = json.load(f)
-        ids = []
-        info = []
-        for item in data:
-            if (item.get("data").get("response") is not None and
-                    item.get("data").get("response").get("restaurants") is not None):
-                info = item.get("data").get("response").get("restaurants")
-                break
+        try:
+            f = open(f"{path}/{file_name}")
+            data = json.load(f)
+            ids = []
+            info = []
+            for item in data:
+                if (item.get("data").get("response") is not None and
+                        item.get("data").get("response").get("restaurants") is not None):
+                    info = item.get("data").get("response").get("restaurants")
+                    break
 
-        for item in info:
-            if item.get("locationId") is not None:
-                ids.append(int(item.get("locationId")))
-
-        location_ids += ids
+            for item in info:
+                if item.get("locationId") is not None:
+                    ids.append(int(item.get("locationId")))
+            location_ids += ids
+        except:
+            print(f"err {city} restaurants")
 
     return location_ids
