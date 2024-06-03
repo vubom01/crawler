68841f4ffa7e97265a1b6d66aa3afae2a87ea0fd
vu.lh <vu.lh@teko.vn>
2024-06-03 10:43:07 +0700

    update constants

--- constants.py
+++ constants.py
@@ -2,13 +2,14 @@ cities = ['Delaware', 'Pennsylvania', 'New_Jersey', 'Georgia', 'Connecticut', 'M
           'South_Carolina', 'New_Hampshire', 'Virginia', 'New_York', 'North_Carolina', 'Rhode_Island', 'Vermont',
           'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine',
           'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'Minnesota', 'Oregon',
-          'Kansas', 'Tây Virginia', 'Nevada', 'Nebraska', 'Colorado', 'Montana', 'Washington', 'Idaho', 'Wyoming',
+          'Kansas', 'West_Virginia', 'Nevada', 'Nebraska', 'Colorado', 'Montana', 'Washington', 'Idaho', 'Wyoming',
           'Utah', 'Oklahoma', 'New_Mexico', 'Arizona', 'Alaska', 'Hawaii']
 
 geo_ids = [28929, 28959, 28951, 28931, 28928, 28942, 28941, 28961, 28950, 28967, 28953, 28954, 28960, 28966, 28938,
            28963, 28956, 28939, 28935, 28945, 28934, 28922, 28940, 28946, 28925, 28943, 28930, 28964, 28936, 28972,
-           28944, 28958, 28937, 28967, 28949, 28948, 28927, 28947, 28968, 28933, 28973, 28965, 28957, 28952, 28924,
-           28923, 28932]
+           28944, 28958,
+           28937, 28971, 28949, 28948, 28927, 28947, 28968, 28933, 28973,
+           28965, 28957, 28952, 28924, 28923, 28932]
 
 # cities = ['Japan']
 # geo_ids = [294232]
